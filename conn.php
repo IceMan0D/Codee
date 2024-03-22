bb4ae9d7c0b445f30438f28606aea75f58569ddf
IceMan0D <jangcom2562@gmail.com>
2024-03-22 20:42:51 +0700

    check

--- conn.php
+++ conn.php
@@ -1,16 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbName = "dataset_code";  //database name
+$host = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$database = "dataset_code"; 
+$port = 3306; 
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
+  $conn = new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   // echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
-
 }
 ?>
